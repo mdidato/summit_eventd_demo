e0075420db64002147414432daf4974ef5cb3e25
mdidato <mdidato@gmail.com>
2023-02-23 09:03:41 -0500

    updated pub ip sku

--- terraform/main.tf
+++ terraform/main.tf
@@ -44,7 +44,6 @@ resource "azurerm_public_ip" "my_terraform_public_ip_aap" {
   location            = var.resource_group_location
   resource_group_name = var.resource_group_name
   allocation_method   = "Dynamic"
-  sku                 = "Standard"
 }
 
 # Create Network Security Group and rule
