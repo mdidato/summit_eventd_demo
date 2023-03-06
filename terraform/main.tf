e878134ee572b43e4be8325fe6bcda74c810083b
mdidato <mdidato@gmail.com>
2023-03-06 12:19:08 -0500

    typo

--- terraform/main.tf
+++ terraform/main.tf
@@ -269,7 +269,7 @@ resource "azurerm_linux_virtual_machine" "my_terraform_vm_monitoring" {
   source_image_reference {
     publisher = "RedHat"
     offer     = "RHEL"
-    sku       = "8.6"
+    sku       = "8_6"
     version   = "latest"
   }
 
